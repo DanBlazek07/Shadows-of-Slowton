c4777ac24da9f31bbfa2b01809e358856cf880ba
Dan <dan.blazek07@gmail.com>
2025-08-25 18:41:23 +0200

    added a player class, so now instead of a square there is a player character

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,6 +1,12 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x,y;
     public int speed;
+    public BufferedImage u1,u2,d1,d2,l1,l2,r1,r2;
+    public String dir;
+    public int spriteCount = 0;
+    public int spriteNum = 1;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.KeyHandler;
 import main.Panel;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     Panel p;
@@ -13,28 +16,96 @@ public class Player extends Entity {
         this.p = p;
         this.k = k;
         defaultV();
+        getPlayerImage();
     }
+
     public void defaultV() {
         x = 96;
         y = 96;
         speed = 8;
+        dir = "down";
     }
-    public void update(){
-        if (k.up){
+
+    public void getPlayerImage() {
+        try {
+            u1 = ImageIO.read(getClass().getResourceAsStream("/player/pu1.png"));
+            u2 = ImageIO.read(getClass().getResourceAsStream("/player/pu2.png"));
+            d1 = ImageIO.read(getClass().getResourceAsStream("/player/pd1.png"));
+            d2 = ImageIO.read(getClass().getResourceAsStream("/player/pd2.png"));
+            l1 = ImageIO.read(getClass().getResourceAsStream("/player/pl1.png"));
+            l2 = ImageIO.read(getClass().getResourceAsStream("/player/pl2.png"));
+            r1 = ImageIO.read(getClass().getResourceAsStream("/player/pp1.png"));
+            r2 = ImageIO.read(getClass().getResourceAsStream("/player/pp2.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update() {
+        if (k.up) {
+            dir = "up";
             y -= speed;
         }
-        if (k.down){
+        if (k.down) {
+            dir = "down";
             y += speed;
         }
-        if (k.left){
+        if (k.left) {
+            dir = "left";
             x -= speed;
         }
-        if (k.right){
+        if (k.right) {
+            dir = "right";
             x += speed;
         }
+        spriteCount++;
+        if (spriteCount > 10) {
+            if (spriteNum == 1){
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCount = 0;
+        }
     }
+
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.BLACK);
-        g2.fillRect(x, y, p.finalTileSize, p.finalTileSize);
+        BufferedImage img = null;
+        switch (dir) {
+            case "up":
+                if (spriteNum == 1) {
+                    img = u1;
+                }
+                if (spriteNum == 2) {
+                    img = u2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    img = d1;
+                }
+                if (spriteNum == 2) {
+                    img = d2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    img = l1;
+                }
+                if (spriteNum == 2) {
+                    img = l2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    img = r1;
+                }
+                if (spriteNum == 2) {
+                    img = r2;
+                }
+                break;
+        }
+        g2.drawImage(img, x, y, p.finalTileSize, p.finalTileSize, null);
     }
 }
